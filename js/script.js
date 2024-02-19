300d7e0cf789b3a042af2156ff8efcddae4f196c
iLudWing <luis_jrd15@hotmail.com>
2024-02-18 19:46:57 -0600

    Update Script

--- js/script.js
+++ js/script.js
@@ -2,27 +2,31 @@ function cifradoCesar(texto, desplazamiento) {
     const alfabeto = 'abcdefghijklmnñopqrstuvwxyz';
     let resultado = '';
 
+    // Ajustar el desplazamiento para casos negativos
+    desplazamiento = (desplazamiento % alfabeto.length + alfabeto.length) % alfabeto.length;
+
     for (let i = 0; i < texto.length; i++) {
         const caracter = texto[i];
-        if (caracter.match(/[a-zñ]/i)) {
+        // Checar si el caracter es una letra y está en el alfabeto (ignorando mayúsculas/minúsculas)
+        if (caracter.toLowerCase() !== caracter.toUpperCase()) {
             const esMayuscula = caracter === caracter.toUpperCase();
             let indiceActual = alfabeto.indexOf(caracter.toLowerCase());
 
             if (indiceActual !== -1) { // Si el caracter está en el alfabeto
-                // Ajustamos el desplazamiento para que siempre sea positivo
-                let indiceCifrado = (indiceActual + desplazamiento + alfabeto.length) % alfabeto.length;
+                let indiceCifrado = (indiceActual + desplazamiento) % alfabeto.length;
                 let caracterCifrado = alfabeto[indiceCifrado];
                 resultado += esMayuscula ? caracterCifrado.toUpperCase() : caracterCifrado;
             } else {
+                // Añadir el caracter tal cual si no está en el alfabeto
                 resultado += caracter;
             }
         } else {
+            // Añadir el caracter tal cual si no es una letra
             resultado += caracter;
         }
     }
     return resultado;
 }
-
 
 function encryptText() {
     var inputText = document.getElementById("inputText").value;
